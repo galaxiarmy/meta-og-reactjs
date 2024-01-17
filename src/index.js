62a486fc686a9fd4059faeb6d0115206b8ceb39b
hanifpw <hanif@kitani.co>
2024-01-17 10:02:32 +0700

    update react-snap

--- src/index.js
+++ src/index.js
@@ -1,18 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 // import { hydrate } from 'react-dom';
 // import { render } from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// without react-snap
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
+ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById("root");
+
+if (rootElement.hasChildNodes()) {
+  ReactDOM.hydrate(<App />, rootElement);
+} else {
+  ReactDOM.render(<App />, rootElement);
+}
+
+// with react-snap
 // const rootElement = document.getElementById("root");
 // if (rootElement.hasChildNodes()) {
 //   hydrate(
